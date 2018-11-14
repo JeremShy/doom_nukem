49f7f085d4b86461a829b2f5a4e506fde52de9d5
Maxime GOUIN <magouin@e3r12p19.42.fr>
2018-11-14 20:06:59 +0100

    pouet

--- srcs/editor/polygon.c
+++ srcs/editor/polygon.c
@@ -67,35 +67,33 @@ uint8_t check_segment(const t_data *data, const t_ivec2 *new_point, const t_ivec
 	j = 0;
 	while (j < data->nb_elements)
 	{
-		if (!data->elements[j].enabled)
+		if (data->elements[j].enabled)
 		{
-			j++;
-			continue ;
-		}
-		polygon = &data->elements[j].polygon;
-		current_edge = 0;
-		while (current_edge < polygon->nb_points)
-		{
-			if (current_edge == polygon->nb_points - 1 && polygon->finished)
+			polygon = &data->elements[j].polygon;
+			current_edge = 0;
+			while (current_edge < polygon->nb_points)
 			{
-				p1 = &polygon->points[current_edge];
-				p2 = &polygon->points[0];
+				if (current_edge == polygon->nb_points - 1 && polygon->finished)
+				{
+					p1 = &polygon->points[current_edge];
+					p2 = &polygon->points[0];
+				}
+				else
+				{
+					p1 = &polygon->points[current_edge];
+					p2 = &polygon->points[current_edge + 1];
+				}
+				if (is_intersect(*p1, *p2, *last_point, *new_point))
+				{
+					printf("Intersection error between {{%d, %d}, {%d, %d}} and {{%d, %d}, {%d, %d}}\n", 
+						p1->x, p1->y,
+						p2->x, p2->y,
+						last_point->x, last_point->y,
+						new_point->x, new_point->y);
+					return (0);
+				}
+				current_edge++;
 			}
-			else
-			{
-				p1 = &polygon->points[current_edge];
-				p2 = &polygon->points[current_edge + 1];
-			}
-			if (is_intersect(*p1, *p2, *last_point, *new_point))
-			{
-				printf("Intersection error between {{%d, %d}, {%d, %d}} and {{%d, %d}, {%d, %d}}\n", 
-					p1->x, p1->y,
-					p2->x, p2->y,
-					last_point->x, last_point->y,
-					new_point->x, new_point->y);
-				return (0);
-			}
-			current_edge++;
 		}
 		j++;
 	}

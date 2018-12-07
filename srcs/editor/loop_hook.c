528d80087541df39d07ebb8baaf6e27c9c7e7862
Anne LANTERI <alanteri@e2r1p14.42.fr>
2018-12-07 17:17:09 +0100

    no leaks on print nums

--- srcs/editor/loop_hook.c
+++ srcs/editor/loop_hook.c
@@ -1,28 +1,37 @@
 #include <editor.h>
 
+void		string_put(t_data *data, int x, int y, int32_t string)
+{
+	char	*num_to_print;
+
+	num_to_print = ft_itoa(string);
+	mlx_string_put(data->mlx.mlx_ptr, data->mlx.win_ptr, x, y, 0x1D1D1D, num_to_print);
+	free(num_to_print);
+}
+
 static void	draw_strings(t_data *data)
 {
 	if (data->input.id_current_element == -1)
 	{
-		// mlx_string_put(data->mlx.mlx_ptr, data->mlx.win_ptr, SMALL_X_CEIL_VALUE_X, SMALL_X_CEIL_VALUE_Y, 0x1D1D1D, ft_itoa(data->input.angle_ceiling.x));
-		// mlx_string_put(data->mlx.mlx_ptr, data->mlx.win_ptr, SMALL_Y_CEIL_VALUE_X, SMALL_Y_CEIL_VALUE_Y, 0x1D1D1D, ft_itoa(data->input.angle_ceiling.y));
-		// mlx_string_put(data->mlx.mlx_ptr, data->mlx.win_ptr, CEIL_HEIGHT_VALUE_X, CEIL_HEIGHT_VALUE_Y, 0x1D1D1D, ft_itoa(data->input.height_ceiling));
-		// mlx_string_put(data->mlx.mlx_ptr, data->mlx.win_ptr, SMALL_X_FLOOR_VALUE_X, SMALL_X_FLOOR_VALUE_Y, 0x1D1D1D, ft_itoa(data->input.angle_floor.x));
-		// mlx_string_put(data->mlx.mlx_ptr, data->mlx.win_ptr, SMALL_Y_FLOOR_VALUE_X, SMALL_Y_FLOOR_VALUE_Y, 0x1D1D1D, ft_itoa(data->input.angle_floor.y));
-		// mlx_string_put(data->mlx.mlx_ptr, data->mlx.win_ptr, FLOOR_HEIGHT_VALUE_X, FLOOR_HEIGHT_VALUE_Y, 0x1D1D1D, ft_itoa(data->input.height_floor));
-		// mlx_string_put(data->mlx.mlx_ptr, data->mlx.win_ptr, LIGHT_VALUE_X, LIGHT_VALUE_Y, 0x1D1D1D, ft_itoa(data->input.light));
+		/*mlx_string_put(data->mlx.mlx_ptr, data->mlx.win_ptr, SMALL_X_CEIL_VALUE_X, SMALL_X_CEIL_VALUE_Y, 0x1D1D1D, ft_itoa(data->input.angle_ceiling.x));
+		mlx_string_put(data->mlx.mlx_ptr, data->mlx.win_ptr, SMALL_Y_CEIL_VALUE_X, SMALL_Y_CEIL_VALUE_Y, 0x1D1D1D, ft_itoa(data->input.angle_ceiling.y));
+		mlx_string_put(data->mlx.mlx_ptr, data->mlx.win_ptr, CEIL_HEIGHT_VALUE_X, CEIL_HEIGHT_VALUE_Y, 0x1D1D1D, ft_itoa(data->input.height_ceiling));
+		mlx_string_put(data->mlx.mlx_ptr, data->mlx.win_ptr, SMALL_X_FLOOR_VALUE_X, SMALL_X_FLOOR_VALUE_Y, 0x1D1D1D, ft_itoa(data->input.angle_floor.x));
+		mlx_string_put(data->mlx.mlx_ptr, data->mlx.win_ptr, SMALL_Y_FLOOR_VALUE_X, SMALL_Y_FLOOR_VALUE_Y, 0x1D1D1D, ft_itoa(data->input.angle_floor.y));
+		mlx_string_put(data->mlx.mlx_ptr, data->mlx.win_ptr, FLOOR_HEIGHT_VALUE_X, FLOOR_HEIGHT_VALUE_Y, 0x1D1D1D, ft_itoa(data->input.height_floor));
+		mlx_string_put(data->mlx.mlx_ptr, data->mlx.win_ptr, LIGHT_VALUE_X, LIGHT_VALUE_Y, 0x1D1D1D, ft_itoa(data->input.light));*/
 	}
 	else if (data->input.mode == SELECTING)
 	{
-		mlx_string_put(data->mlx.mlx_ptr, data->mlx.win_ptr, SECTOR_NUM_X, SECTOR_NUM_Y, 0x1D1D1D, ft_itoa(data->input.id_current_element));
-		mlx_string_put(data->mlx.mlx_ptr, data->mlx.win_ptr, SMALL_X_CEIL_VALUE_X, SMALL_X_CEIL_VALUE_Y, 0x1D1D1D, ft_itoa(data->elements[data->input.id_current_element].angle_ceiling.x));
-		mlx_string_put(data->mlx.mlx_ptr, data->mlx.win_ptr, SMALL_Y_CEIL_VALUE_X, SMALL_Y_CEIL_VALUE_Y, 0x1D1D1D, ft_itoa(data->elements[data->input.id_current_element].angle_ceiling.y));
-		mlx_string_put(data->mlx.mlx_ptr, data->mlx.win_ptr, CEIL_HEIGHT_VALUE_X, CEIL_HEIGHT_VALUE_Y, 0x1D1D1D, ft_itoa(data->elements[data->input.id_current_element].height_ceiling));
-		mlx_string_put(data->mlx.mlx_ptr, data->mlx.win_ptr, SMALL_X_FLOOR_VALUE_X, SMALL_X_FLOOR_VALUE_Y, 0x1D1D1D, ft_itoa(data->elements[data->input.id_current_element].angle_floor.x));
-		mlx_string_put(data->mlx.mlx_ptr, data->mlx.win_ptr, SMALL_Y_FLOOR_VALUE_X, SMALL_Y_FLOOR_VALUE_Y, 0x1D1D1D, ft_itoa(data->elements[data->input.id_current_element].angle_floor.y));
-		mlx_string_put(data->mlx.mlx_ptr, data->mlx.win_ptr, FLOOR_HEIGHT_VALUE_X, FLOOR_HEIGHT_VALUE_Y, 0x1D1D1D, ft_itoa(data->elements[data->input.id_current_element].height_floor));
-		mlx_string_put(data->mlx.mlx_ptr, data->mlx.win_ptr, LIGHT_VALUE_X, LIGHT_VALUE_Y, 0x1D1D1D, ft_itoa(data->elements[data->input.id_current_element].light));
-	}	
+		string_put(data, SECTOR_NUM_X, SECTOR_NUM_Y, data->input.id_current_element);
+		string_put(data, SMALL_X_CEIL_VALUE_X, SMALL_X_CEIL_VALUE_Y, data->elements[data->input.id_current_element].angle_ceiling.x);
+		string_put(data, SMALL_Y_CEIL_VALUE_X, SMALL_Y_CEIL_VALUE_Y, data->elements[data->input.id_current_element].angle_ceiling.y);
+		string_put(data, CEIL_HEIGHT_VALUE_X, CEIL_HEIGHT_VALUE_Y, data->elements[data->input.id_current_element].height_ceiling);
+		string_put(data, SMALL_X_FLOOR_VALUE_X, SMALL_X_FLOOR_VALUE_Y, data->elements[data->input.id_current_element].angle_floor.x);
+		string_put(data, SMALL_Y_FLOOR_VALUE_X, SMALL_Y_FLOOR_VALUE_Y, data->elements[data->input.id_current_element].angle_floor.y);
+		string_put(data, FLOOR_HEIGHT_VALUE_X, FLOOR_HEIGHT_VALUE_Y, data->elements[data->input.id_current_element].height_floor);
+		string_put(data, LIGHT_VALUE_X, LIGHT_VALUE_Y, data->elements[data->input.id_current_element].light);
+	}
 }
 
 static void	draw_texture(t_data *data)

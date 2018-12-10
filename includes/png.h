9af926623341c7b86e84ca377b4e5130d4d0e9d9
Jeremy CAMHI <jcamhi@student.42.fr>
2018-12-10 14:46:47 +0100

    png.h

--- includes/png.h
+++ includes/png.h
@@ -11,69 +11,7 @@
 # define IDAT 0x49444154
 # define IEND 0x49454E44
 
-/*
-**
-** In other words, if one were to print out the compressed data as
-** a sequence of bytes, starting with the first byte at the
-** *right* margin and proceeding to the *left*, with the most-
-** significant bit of each byte on the left as usual, one would be
-** able to parse the result from right to left, with fixed-width
-** elements in the correct MSB-to-LSB order and Huffman codes in
-** bit-reversed order (i.e., with the first bit of the code in the
-** relative LSB position).
-**
-** explications :
-** Si on ecrivait une sequence de bytes de droite a gauche, bytes par bytes, avec les bits places "normalement" (bits le plus signifiants en premier) :
-** 31 30 29 28 27 26 25 24		...		15 14 13 12 11 10 09 08		07 06 05 04 03 02 01 00
-**										^
-**										|
-**										+ ------- Cet octet sera un huffman code
-**
-** Alors en le lisant de droite a gauche bits par bits, et en inversant uniquement les huffman codes on aurait le resultat encodé correctemnt :
-** 00 01 02 03 04 05 06 07		15 14 13 12 11 10 09 08		...		24 25 26 27 28 29 30 31
-**								^
-**								|
-**								+ ------- Inversé car huffman code (dans l'exemple)
-
-
-
-16	
-
-111	001
-
-
-
-
-
-00 01 02 03 04 05 06 07
-
-07 06 05 04 03 02 01 xx		0f 0e 0d 0c 0b 0a 09 08		xx xx xx xx xx xx xx 10
-
-
-100f0e0d0c0b0a09 0807060504030201
-
-EF CD AB 89
-[0] EF
-[1] CD
-
-AB 89
-[0] AB
-
-7  6  5  4  3  2  1  0
-xx xx 01 00 xx xx xx xx
-00 00 00 00 xx xx 01 00
-
-
-
-xx xx 07 06 05 04 03 02		01 00 xx xx xx ...
-
-
-
-xx xx 00 01 02 03 xx xx		xx xx xx xx xx ...
- 1  1  1  0  1  1  0  0
- 0  0  1  1  0  1  1 
-
-*/
+typedef struct s_ivec2 t_ivec2;
 
 struct	s_png_ihdr {
 	uint32_t	width;

7140babed559e7e56826d5201db81d4c93253239
Jeremy CAMHI <jcamhi@student.42.fr>
2019-01-06 17:17:37 +0100

    ajout de commentaires

--- srcs/game/intersect_edges.c
+++ srcs/game/intersect_edges.c
@@ -9,6 +9,7 @@ static void	ptr_swap(void **p1, void **p2)
 	*p2 = tmp;
 }
 
+// p1 must be a point in front of us, p2 may or may not be behind us. p1 and p2 belong to e1
 void		get_p1_p2(t_data *data, t_edge *e1, t_vec2	*p1, t_vec2	*p2)
 {
 	float tmp;
@@ -43,9 +44,9 @@ t_edge		*is_edges_intersect(t_data *data, t_edge *e1, t_edge *e2)
 
 	printf("p1 = {{%f, %f}, {%f, %f}} et p2 = {{%f, %f}, {%f, %f}}\n", e1->p1->x, e1->p1->y, e1->p2->x, e1->p2->y, e2->p1->x, e2->p1->y, e2->p2->x, e2->p2->y);
 	get_p1_p2(data, e1, &p1, &p2);
-	dir = ft_vec2_sub(p2, p1);
-	printf("*e2->p1 = {%f, %f}\n", e2->p1->x, e2->p1->y);
-	printf("p1 = {%f, %f}\n", p1.x, p1.y);
+
+	dir = ft_vec2_sub(p2, p1); // direction of e1
+
 	v1 = ft_vec2_sub(*e2->p1, p1);
 	printf("v1 = {%f, %f}\n", v1.x, v1.y);
 	v2 = ft_vec2_sub(*e2->p2, p1);
